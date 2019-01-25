81e76472465f234e8872f4efeef2d0f978bf6665
Nate Goldman <ungoldman@gmail.com>
2019-01-24 18:20:13 -0800

    last try

--- bin.js
+++ bin.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-var path = require('path')
-require('budo')(path.join(__dirname, './solve.js'), {
+require('budo')(require.resolve('./solve.js'), {
   stream: process.stdout,
   live: true,
   open: true
